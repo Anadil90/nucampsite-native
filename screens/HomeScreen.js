50d03f4af3d867cbd7ccd3b9b745eb0448500946
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-11-08 00:57:51 +0100

    Home screen animation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,32 +1,27 @@
-
-import { useSelector} from 'react-redux';
-import { ScrollView, Text, View } from 'react-native';
+import { useEffect, useRef } from 'react';
+import { Text, View, Animated } from 'react-native';
 import { Card } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from '../components/LoadingComponent';
 
 const FeaturedItem = (props) => {
-
     const { item } = props;
 
-    if(props.isLoading) {
-        return (
-            <Loading />
-        )
+    if (props.isLoading) {
+        return <Loading />;
     }
-
-    if(props.errMsg) {
+    if (props.errMess) {
         return (
             <View>
-                <Text>{props.errMsg}</Text>
+                <Text>{props.errMess}</Text>
             </View>
-        )
+        );
     }
-
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }}>
-                <Card.Image source={{ uri: baseUrl + item.image}}>
+                <Card.Image source={{ uri: baseUrl + item.image }}>
                     <View style={{ justifyContent: 'center', flex: 1 }}>
                         <Text
                             style={{
@@ -48,31 +43,43 @@ const FeaturedItem = (props) => {
 
 const HomeScreen = () => {
     const campsites = useSelector((state) => state.campsites);
+    const promotions = useSelector((state) => state.promotions);
     const partners = useSelector((state) => state.partners);
-    const promotions = useSelector((state) => state.promotions)
+    const scaleValue = useRef(new Animated.Value(0)).current;
+    const scaleAnimation = Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 1500,
+        useNativeDriver: true
+    });
 
     const featCampsite = campsites.campsitesArray.find((item) => item.featured);
-    const featPromotion = promotions.promotionsArray.find((item) => item.featured);
+    const featPromotion = promotions.promotionsArray.find(
+        (item) => item.featured
+    );
     const featPartner = partners.partnersArray.find((item) => item.featured);
 
+    useEffect(() => {
+        scaleAnimation.start();
+    }, []);
+
     return (
-        <ScrollView>
-            <FeaturedItem 
-            item={featCampsite} 
-            isLoading={campsites.isLoading}
-            errMsg={campsites.errMsg}
+        <Animated.ScrollView style={{ transform: [{ scale: scaleValue }] }}>
+            <FeaturedItem
+                item={featCampsite}
+                isLoading={campsites.isLoading}
+                errMess={campsites.errMess}
             />
-            <FeaturedItem 
-            item={featPromotion} 
-            isLoading={promotions.isLoading}
-            errMsg={promotions.errMsg}
+            <FeaturedItem
+                item={featPromotion}
+                isLoading={promotions.isLoading}
+                errMess={promotions.errMess}
             />
-            <FeaturedItem 
-            item={featPartner} 
-            isLoading={partners.isLoading}
-            errMsg={partners.errMsg}
+            <FeaturedItem
+                item={featPartner}
+                isLoading={partners.isLoading}
+                errMess={partners.errMess}
             />
-        </ScrollView>
+        </Animated.ScrollView>
     );
 };
 
