7dbd67af8425b7e30723541387f01f9fbb9f1bb2
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-11-11 13:50:44 +0100

    User can capture and pick image for user profile picture

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
 import { CheckBox, Input, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as ImagePicker from 'expo-image-picker'; 
+import { baseUrl } from '../shared/baseUrl';
+import logo from '../assets/images/logo.png';
 
 const LoginTab = ({navigation}) => {
     const [username, setUsername] = useState('');
@@ -91,6 +94,7 @@ const RegisterTab = () => {
     const [lastName, setLastname] = useState('');
     const [email, setEmail] = useState('');
     const [remember, setRemember] = useState(false);
+    const [imageUrl, setImageUrl] = useState(baseUrl + 'images/logo.png')
 
     const handleRegister = () => {
         const userInfo = {
@@ -103,7 +107,7 @@ const RegisterTab = () => {
         };
         console.log(JSON.stringify(userInfo))
 
-        if (remember) {
+        if(remember) {
             SecureStore.setItemAsync(
                 'userinfo',
                 JSON.stringify({
@@ -118,9 +122,33 @@ const RegisterTab = () => {
         }
     }
 
+    const getImageFromCamera = async () => {
+        const cameraPermission =
+            await ImagePicker.requestCameraPermissionsAsync();
+
+        if (cameraPermission.status === 'granted') {
+            const capturedImage = await ImagePicker.launchCameraAsync({
+                allowsEditing: true,
+                aspect: [1, 1]
+            });
+            if (capturedImage.assets) {
+                console.log(capturedImage.assets[0]);
+                setImageUrl(capturedImage.assets[0].uri);
+            }
+        }
+    };
+
     return (
         <ScrollView>
             <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={{ uri: imageUrl }}
+                        loadingIndicatorSource={logo}
+                        style={styles.image}
+                    />
+                    <Button title='Camera' onPress={getImageFromCamera} />
+                </View>
                 <Input
                     placeholder='Username'
                     leftIcon={{ type: 'font-awesome', name: 'user-o' }}
@@ -155,7 +183,7 @@ const RegisterTab = () => {
                 />
                 <Input
                     placeholder='Email'
-                    leftIcon={{ type: 'font-awesome', name: 'evelope-o' }}
+                    leftIcon={{ type: 'font-awesome', name: 'envelope-o' }}
                     onChangeText={(text) => setEmail(text)}
                     value={email}
                     containerStyle={styles.formInput}
@@ -257,6 +285,17 @@ const styles = StyleSheet.create({
         margin: 20,
         marginRight: 40,
         marginLeft: 40
+    },
+    imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        margin: 10
+    },
+    image: {
+        width: 60,
+        height: 60
     }
 });
 
